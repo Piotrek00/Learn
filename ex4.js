8c7c7e87ada7f761287e89b3a6b79a53329817d3
Piotrek00 <kotarbap93@gmail.com>
2023-01-27 23:08:16 +0100

    Finished function part

--- ex4.js
+++ ex4.js
@@ -1,21 +1,52 @@
 // Funkcje
 
 
-function funkcjaDodawania(a, b) {
-    let c = a + b;
-    console.log('Wynik : ', c)
+// function funkcjaDodawania(a, b) {
+//     let c = a + b;
+//     console.log('Wynik : ', c)
+// }
+
+// funkcjaDodawania(8, 3);
+// funkcjaDodawania(10, 20);
+// funkcjaDodawania(100, 200) 
+
+
+// let x = 10;
+// let y = 20;
+
+// if (x >= 10) {
+//     funkcjaDodawania(x, y);
+// }
+
+// let result = funkcjaDodawania(x, y)
+
+// let dzielenie = function (a, b) {
+//     return a/b;
+// }
+
+// let dzielenieWynik = dzielenie(10, 2);
+// console.log('Wynik dzielenia 2 liczb ', dzielenieWynik);
+
+
+
+// function odejmowanie(a, b) {
+//   let c = a - b;
+//   console.log('Wynik : ', c)
+// }
+// odejmowanie(10, 5)
+
+
+function wartosc(a) {
+
+    if (a < 0) {
+     let c = a * -1;
+     console.log('Wartość bezwzględna: ', c); 
+    }
+    else {
+        console.log('Wartość bezwzględna: ', a)
+    }
+
 }
 
-funkcjaDodawania(8, 3);
-funkcjaDodawania(10, 20);
-funkcjaDodawania(100, 200) 
 
-
-let x = 10;
-let y = 20;
-
-if (x >= 10) {
-    funkcjaDodawania(x, y);
-}
-
-let result = funkcjaDodawania(x, y)
+wartosc(-1)
