3b7cc8f7f638bd41d62168144c6a51ae07367698
Piotrek00 <kotarbap93@gmail.com>
2023-01-28 14:32:45 +0100

    Object Exercises

--- ex5.js
+++ ex5.js
@@ -1,9 +1,43 @@
 // Obiekty
 
-let car1 = {
-    brand: 'Chevrolet',
-    model: 'Camaro',
-    year: 1997,
-};
+// let car1 = {
+//     brand: 'Chevrolet',
+//     model: 'Camaro',
+//     year: 1997,
+// };
 
-console.log(car1.brand, car1.model, car1.year)
+// console.log(car1.brand, car1.model, car1.year)
+
+// let rocket = {
+//     brand: 'SpaceX',
+//     model: 'Falcon',
+//     topSpeed: 30000,
+
+//     printInfo: function() {
+//         console.log(this.brand, this.model, this.topSpeed);
+//     },   
+
+//     printTopSpeed: function() {
+//         console.lot('Maksymalna prędkość to: ', this.topSpeed);
+//     }
+// };
+
+// rocket.printInfo();
+
+
+//EXERCISES
+
+let person = {
+    personName: 'Piotr',
+    personSurname: 'Kotarba',
+    personAge: 29,
+    town: 'Rytro',
+    postalCode: 33-343,
+    country: 'Poland',
+
+    getName: function(){
+        console.log('Dane uzytkownika: ', this.personName + ' ', this.personSurname)
+    }
+}
+
+person.getName();
