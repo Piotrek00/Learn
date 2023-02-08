2c55d5950c9f5a16e6f5171a8e7aa94e63cccd71
Piotrek00 <kotarbap93@gmail.com>
2023-02-08 19:50:21 +0100

    Typeof

---
+++ ex11.js
@@ -0,0 +1,27 @@
+let data = 15;
+let info = typeof data; // zwraca typ przechowywanych danych
+
+console.log(info); // number
+
+if (info == "number") {
+  console.log("Liczba");
+}
+
+data = "tekst";
+
+if (data == "number") {
+  console.log("Liczba");
+} else {
+  console.log("To nie liczba");
+}
+// Zwracane wartosci przez typeof
+
+console.log(typeof 20); //number
+console.log(typeof "tekst"); // string
+console.log(typeof "40"); // string
+console.log(typeof true); // boolean
+console.log(typeof false); // boolean
+console.log(typeof { a: 5 }); // object
+console.log(typeof [0]); // object
+console.log(typeof null); // OBJECT !!!!!!!!
+console.log(typeof undefined); // undefined
